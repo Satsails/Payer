b4fbfbde25a80c5fb29eb25b490acdbd9974c48c
joaolobo <joao.lobom@gmail.com>
2024-06-21 16:11:05 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use lwk_cli::{
+    inner_main, AssetSubCommandsEnum, Cli, ServerSubCommandsEnum, SignerSubCommandsEnum,
+    WalletSubCommandsEnum,
+};
+
 fn main() {
     println!("Hello, world!");
 }
