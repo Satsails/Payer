73492831e80f6843aacaf8f7f4dc6ca02b88ea4d
joaolobo <joao.lobom@gmail.com>
2024-06-21 16:18:47 +0100

    todo

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use lwk_cli::{
     WalletSubCommandsEnum,
 };
 
+// This app must be connect to firebaes and get all address of all users and all sats to be payed.
+// then it must connect to a hardwere wallet and build a transaction and sign it and broadcast it.
+
 fn main() {
     println!("Hello, world!");
 }
